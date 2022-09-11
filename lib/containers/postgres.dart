a6a8d422c7e981753b82ac022d8149f2bb955cc6
osaxma <46427323+osaxma@users.noreply.github.com>
2022-09-11 16:36:33 +0300

    Add the ability to configure the postgres container
    This includes mounting `pg_hba.conf` or `postgresql.conf`. Also the user can pass indivisual configs using `configurations`

--- lib/containers/postgres.dart
+++ lib/containers/postgres.dart
@@ -2,6 +2,18 @@ import 'package:docker_process/docker_process.dart';
 
 export 'package:docker_process/docker_process.dart';
 
+/// Starts a postgres container with the given [name] using the given
+/// [imageName] and [version].
+///
+/// Use [postgresqlConfPath] or [pgHbaConfPath] to mount `postgresql.conf`
+/// or `pg_hba.conf` on the container, respectively.
+///
+/// Use [configuraions] to pass list of configs to the `postgres` image. For
+/// example: ['shared_buffers=256MB', 'max_connections=200']. As shown, each 
+/// item in the list must contain the parameter and its assignment in as a single
+/// item. 
+///
+/// For other options, please refer to [DockerProcess.start].
 Future<DockerProcess> startPostgres({
   required String name,
   required String version,
@@ -12,20 +24,52 @@ Future<DockerProcess> startPostgres({
   String? pgDatabase,
   int pgPort = 5432,
   bool? cleanup,
+  String? postgresqlConfPath,
+  String? pgHbaConfPath,
+  List<String>? configuraions,
 }) async {
   var ipv4 = false;
 
+  final dockerArgs = <String>[];
+  final imageArgs = <String>[];
+
+  if (configuraions != null) {
+    for (var config in configuraions) {
+      imageArgs.add('-c');
+      imageArgs.add(config);
+    }
+  }
+
+  // see Database Configuration section at official image page:
+  // https://hub.docker.com/_/postgres/
+  if (postgresqlConfPath != null) {
+    dockerArgs.add('-v');
+    dockerArgs.add('$postgresqlConfPath:/etc/postgresql/postgresql.conf');
+
+    imageArgs.add('-c');
+    imageArgs.add('config_file=/etc/postgresql/postgresql.conf');
+  }
+
+  if (pgHbaConfPath != null) {
+    dockerArgs.add('-v');
+    dockerArgs.add('$pgHbaConfPath:/etc/postgresql/pg_hba.conf');
+
+    imageArgs.add('-c');
+    imageArgs.add('hba_file=/etc/postgresql/pg_hba.conf');
+  }
+
   return await DockerProcess.start(
     name: name,
+    dockerArgs: dockerArgs,
     image: '$imageName:$version',
+    imageArgs: imageArgs,
     network: network,
     hostname: name,
     ports: ['$pgPort:5432'],
     cleanup: cleanup,
     readySignal: (line) {
       ipv4 |= line.contains('listening on IPv4 address "0.0.0.0", port 5432');
-      return ipv4 &&
-          line.contains('database system is ready to accept connections');
+      return ipv4 && line.contains('database system is ready to accept connections');
     },
     environment: {
       if (pgUser != null) 'POSTGRES_USER': pgUser,
--- test/postgres_test.dart
+++ test/postgres_test.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:docker_process/containers/postgres.dart';
+import 'package:path/path.dart' as p;
 import 'package:postgres/postgres.dart';
 import 'package:test/test.dart';
+
+final _samplesDir = p.join(Directory.current.path, 'test', 'samples');
 
 void main() {
   group('postgres', () {
@@ -26,6 +31,130 @@ void main() {
         password: 'postgres',
       );
       await c.open();
+      await c.close();
+    });
+  });
+
+  group('mounting conf files', () {
+    DockerProcess? dp1;
+    DockerProcess? dp2;
+
+    tearDownAll(() async {
+      await dp1?.stop();
+      await dp2?.stop();
+      await dp1?.kill();
+      await dp2?.kill();
+    });
+
+    test('mount custom postgresql.conf', () async {
+      final postgresqlConfPath = p.join(_samplesDir, 'sample_postgresql.conf');
+      dp1 = await startPostgres(
+        name: 'test_postgresql_conf_mounting',
+        version: 'latest',
+        cleanup: true,
+        pgPort: 5432,
+        postgresqlConfPath: postgresqlConfPath,
+      );
+
+      // confirm sample_postgresql.conf is mounted correctly
+      final res = await dp1!.exec([
+        'cat',
+        '/etc/postgresql/postgresql.conf',
+      ]);
+
+      final file = res.stdout as String;
+
+      expect(file.startsWith('# sample_postgresql.conf'), true);
+    });
+
+    test('mount custom pg_hba.conf', () async {
+      final pgHbaConfPath = p.join(_samplesDir, 'sample_pg_hba.conf');
+
+      dp2 = await startPostgres(
+        name: 'test_pg_hba_conf_mounting',
+        version: 'latest',
+        cleanup: true,
+        // this would conflict with the other test in the group 
+        // so it's a different number
+        pgPort: 54321, 
+        pgHbaConfPath: pgHbaConfPath,
+      );
+      final res = await dp2!.exec([
+        'cat',
+        '/etc/postgresql/pg_hba.conf',
+      ]);
+
+      final file = res.stdout as String;
+
+      expect(file.startsWith('# sample_pg_hba.conf'), true);
+    });
+  });
+
+  group('mounting postgresql.conf is effective', () {
+    DockerProcess? dp;
+
+    tearDownAll(() async {
+      await dp?.stop();
+      await dp?.kill();
+    });
+
+    // in the `sample_postgresql.conf`, the `wal_level` was changed to `logical`
+    // here we test if this has taken effect after mounting the sample conf.
+    test('wal_level should be logical', () async {
+      final postgresqlConfPath = p.join(_samplesDir, 'sample_postgresql.conf');
+      dp = await startPostgres(
+        name: 'check_wal_level',
+        version: 'latest',
+        cleanup: true,
+        pgPort: 5432,
+        postgresqlConfPath: postgresqlConfPath,
+      );
+
+      // confirm sample_postgresql.conf is mounted correctly
+      final c = PostgreSQLConnection(
+        'localhost',
+        5432,
+        'postgres',
+        username: 'postgres',
+        password: 'postgres',
+      );
+
+      await c.open();
+      final res = await c.query('show wal_level');
+      expect(res.first.first, 'logical');
+      await c.close();
+    });
+  });
+
+  group('test postgres configuration arguments', () {
+    DockerProcess? dp;
+
+    tearDownAll(() async {
+      await dp?.stop();
+      await dp?.kill();
+    });
+
+    test('Adding configuration arguments is reflected on the image', () async {
+      dp = await startPostgres(
+        name: 'test_postgresql_conf_mounting',
+        version: 'latest',
+        cleanup: true,
+        pgPort: 5432,
+        configuraions: ['max_connections=42'],
+      );
+
+      // confirm sample_postgresql.conf is mounted correctly
+      final c = PostgreSQLConnection(
+        'localhost',
+        5432,
+        'postgres',
+        username: 'postgres',
+        password: 'postgres',
+      );
+
+      await c.open();
+      final res = await c.query('show max_connections');
+      expect(res.first.first, '42');
       await c.close();
     });
   });
